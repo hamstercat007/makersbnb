30ee00c10e0850ce29855d68a208cf100f791869
luke <luke44@gmail.com>
2021-10-18 16:54:40 +0100

    Added date argument and test

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,9 +1,10 @@
 class Venue
-  attr_reader :host_user_id, :name, :description, :price_per_night
-  def initialize(host_user_id, name, description, price_per_night)
+  attr_reader :host_user_id, :name, :description, :price_per_night, :date
+  def initialize(host_user_id, name, description, price_per_night, date)
     @host_user_id = host_user_id
     @name = name
     @description = description
     @price_per_night = price_per_night
+    @date = date
   end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,24 +2,24 @@ require './lib/venue'
 
 describe Venue do
   it 'initialises with host_user_id' do
-    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000")
+    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000", "12th May 2021")
     expect(v.host_user_id).to eq "5144"
   end
   it 'initialises with name' do
-    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000")
+    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000", "12th May 2021")
     expect(v.name).to eq "Penthouse 5"
   end
   it 'initialises description' do
-    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000")
+    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000", "12th May 2021")
     expect(v.description).to eq "Riverside 4 bed"
   end
   it 'initialises with price_per_night' do
-    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000")
+    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000", "12th May 2021")
     expect(v.price_per_night).to eq "1000"
   end
   it 'initialises with price_per_night' do
-    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000", )
-    expect(v.price_per_night).to eq "1000"
+    v = Venue.new("5144", "Penthouse 5", "Riverside 4 bed", "1000", "12th May 2021")
+    expect(v.date).to eq "12th May 2021"
   
   end
 end
