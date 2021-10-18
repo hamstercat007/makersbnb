901783d44b34f4df23e6a9a121379e9c078ce75f
Lisa Mac <lisa@MacBook-Air-2.local>
2021-10-18 21:50:12 +0100

    Creating connection successfully

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,3 +1,5 @@
+require 'pg'
+
 class Venue
   attr_reader :host_user_id, :name, :description, :price_per_night
   def initialize(host_user_id, name, description, price_per_night)
@@ -5,5 +7,11 @@ class Venue
     @name = name
     @description = description
     @price_per_night = price_per_night
+  end
+
+  def self.all
+    connection = PG.connect(dbname: 'makersbnb')
+    result = connection.exec("SELECT * FROM venues;")
+    result.map { |venue| venue['name'] }
   end
 end
---
+++ spec/venues_db_spec.rb
@@ -0,0 +1,14 @@
+require 'venue'
+
+describe Venue do
+
+  describe '.all' do
+    it 'returns a list of venues' do
+      venues = Venue.all
+
+      expect(venues).to include "Penthouse"
+      expect(venues).to include "Villa"
+      expect(venues).to include "Chalet"
+    end
+  end 
+end
