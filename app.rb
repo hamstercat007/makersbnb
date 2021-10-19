b29566a59ff2d9737f42ef41cfb8f1e2a1d69228
Lisa Mac <lisa@MacBook-Air-2.local>
2021-10-19 16:17:31 +0100

    Adding new venues path

--- app.rb
+++ app.rb
@@ -11,7 +11,15 @@ class Makersbnb < Sinatra::Base
     @venues = Venue.all
     erb:index
   end
+  
+  get '/venues/new' do
+    erb :'venues/new'
+  end 
 
+  post '/venues' do
+    Venue.create(name: params[:venue_name], description: params[:description], user_id: params[:user_id], price_per_night: params[:price_per_night] )
+    redirect '/index'
+  end 
 
   run! if app_file == $0
 end
