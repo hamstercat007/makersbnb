3f7b79543d17464009c20658cdd584c894649a70
Tim Turvey <tturvey@hotmail.co.uk>
2021-10-20 12:30:29 +0100

    Login and create account buttons added to front page

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/venue'
 
 class Makersbnb < Sinatra::Base
   configure :development do
@@ -7,11 +8,45 @@ class Makersbnb < Sinatra::Base
   end 
 
   get '/' do
-    p ENV
-    @venues = Venue.all
     erb:index
   end
 
+  post '/user/login' do
+    # useless assignment to variables at the moment. 
+    # user_is and password need to be integrated into a session.
+    user_id = params[:user_id]
+    password = params[:password]
+    erb:venues_2
+  end
+
+  post '/user/signup' do
+    erb:user_create
+  end
+
+  get '/user/create' do
+    new_user_id = params[:user_id]
+    new_user_password = params[:password]
+    # TBC - add new_user_id and new_user_password to the users database.
+    erb:index
+  end
+
+  
+
+
+
+  # get '/venues' do
+  #   @venues = Venue.all
+  #   erb:venues_2
+  # end
+  
+  # get '/venues/new' do
+  #   erb :'venues/new'
+  # end 
+
+  # post '/venues' do
+  #   Venue.create(user_id: params[:user_id], name: params[:venue_name], description: params[:description], price_per_night: params[:price_per_night], date: params[:date])
+  #   redirect '/index'
+  # end 
 
   run! if app_file == $0
 end
--- lib/venue.rb
+++ lib/venue.rb
@@ -1,22 +1,31 @@
 require 'pg'
 
 class Venue
-  attr_reader :host_user_id, :name, :description, :price_per_night
-  def initialize(host_user_id, name, description, price_per_night)
-    @host_user_id = host_user_id
+  attr_reader :venue_id, :user_id, :name, :description, :price_per_night, :date
+  def initialize(venue_id:, user_id:, name:, description:, price_per_night:, date:)
+    @venue_id = venue_id
+    @user_id = user_id
     @name = name
     @description = description
     @price_per_night = price_per_night
+    @date = date
   end
 
   def self.all
-
     if ENV['ENVIRONMENT'] == 'test'
-    connection = PG.connect(dbname: 'makersbnb_test')
+      connection = PG.connect(dbname: 'makersbnb_test')
     else
-    connection = PG.connect(dbname: 'makersbnb')
+      connection = PG.connect(dbname: 'makersbnb')
     end
     result = connection.exec("SELECT * FROM venues;")
     result.map { |venue| venue['name'] }
+  end
+
+  def self.create(user_id:, name:, description:, price_per_night:, date:)
+    host_user_id = user_id
+    connection = PG.connect(dbname: 'makersbnb')
+    result = connection.exec("INSERT INTO venues (host_user_id, name, description, price_per_night, date) VALUES('#{host_user_id}', '#{name}', '#{description}', '#{price_per_night}', '#{date}') RETURNING venue_id, host_user_id, name, description, price_per_night, date;")
+    Venue.new(venue_id: result[0]['venue_id'], user_id: result[0]['user_id'], name: result[0]['name'], description: result[0]['description'], price_per_night: result[0]['price_per_night'], date: result[0]['date'])
+    
   end
 end
