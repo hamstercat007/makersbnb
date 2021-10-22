ddb055c7ab5f26329176d88888f6304258d63ff1
Tim Turvey <89144691+TTurvey@users.noreply.github.com>
2021-10-22 10:41:41 +0100

    Update user.rb

--- lib/user.rb
+++ lib/user.rb
@@ -25,5 +25,40 @@ class User
     result = DatabaseConnection.query("INSERT INTO users (email, password, mobile_number) VALUES('#{email}', '#{encrypted_password}', '#{mobile_number}') RETURNING user_id, email, mobile_number;")
     User.new(user_id: result[0]['user_id'], email: result[0]['email'], mobile_number: result[0]['mobile_number'])
   end
+  
+  def self.find(id:)
+    return nil unless id
+    result = DatabaseConnection.query(
+      "SELECT * FROM users WHERE user_id = $1", [id]
+    )
+    User.new(
+      user_id: result[0]['user_id'],
+      email: result[0]['email'],
+      mobile_number: result[0]['mobile number']
+    )
+  end
 
+  def self.authenticate(email:, password:)
+    result = DatabaseConnection.query(
+      "SELECT * FROM users WHERE email = $1", [email]
+    )
+    return unless result.any?
+    return unless BCrypt::Password.new(result[0]['password']) == password
+    User.new(user_id: result[0]['user_id'], email: result[0]['email'], mobile_number: result[0]['mobile number'])
+  end
+  
+  def requested_bookings #only hosts do this
+    connection = PG.connect(dbname: 'makersbnb')
+    # the below is combining tables and adding columns, not getting a subset
+    result = connection.exec("SELECT * FROM venues, bookings WHERE host_user_id = '#{@user_id}' AND venues.venue_id::INTEGER = bookings.venue_id::INTEGER AND confirmed::INTEGER = 0;")
+    result.map do |venue|
+      Venue.new(venue_id: result[0]['venue_id'], user_id: result[0]['user_id'], name: result[0]['name'], description: result[0]['description'], price_per_night: result[0]['price_per_night'], date: result[0]['date'])
+    end
+  end
+
+  def self.confirm_booking(venue_id) #only hosts do this
+    connection = PG.connect(dbname: 'makersbnb')
+    connection.exec("UPDATE bookings SET confirmed = '1' WHERE venue_id = '#{venue_id}';")
+  end
+  
 end
